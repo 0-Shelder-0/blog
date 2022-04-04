64a3f853b45c4b3ebbb8661104c6401a8fb37ae2
Vladislav Piligrimov <vladpiligrimov@gmail.com>
2022-04-04 22:48:26 +0500

    added crud operations

---
+++ modules/user/views/comment/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Comment */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="comment-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'text')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'is_deleted')->checkbox() ?>
+
+    <?= $form->field($model, 'created_on')->textInput() ?>
+
+    <?= $form->field($model, 'user_id')->textInput() ?>
+
+    <?= $form->field($model, 'post_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
